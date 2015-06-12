8ebfa4a0aae8580d407092af39a4281fa31535d2
jannakha <j_malikova@yahoo.com>
2015-06-12 15:35:45 +1000

    added aws pubslish task

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ var merge = require('merge-stream');
 var path = require('path');
 var fs = require('fs');
 var glob = require('glob');
+var awspublish = require('gulp-awspublish');
 
 var AUTOPREFIXER_BROWSERS = [
   'ie >= 10',
@@ -237,6 +238,33 @@ gulp.task('default', ['clean'], function (cb) {
     ['jshint', 'images', 'fonts', 'html'],
     'vulcanize', 'precache',
     cb);
+});
+
+gulp.task('publish', function() {
+
+  // create a new publisher using S3 options
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+  //http://marcocarag.com/2014/06/18/migrating-away-from-github-pages-and-changing-task-runners-just-cause/
+  var publisher = awspublish.create(JSON.parse(fs.readFileSync('./aws.json')));
+
+  // define custom headers
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+  };
+
+  return gulp.src('./dist/*')
+    // gzip, Set Content-Encoding headers and add .gz extension
+    .pipe(awspublish.gzip({ ext: '.gz' }))
+
+    // publisher will add Content-Length, Content-Type and headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers))
+
+    // create a cache file to speed up consecutive uploads
+    .pipe(publisher.cache())
+
+    // print upload updates to console
+    .pipe(awspublish.reporter());
 });
 
 // Load tasks for web-component-tester
