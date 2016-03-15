6b9adef70b43d196afba283b64ae8407ab510fc9
--global <jwwisgerhof@gmail.com>
2016-03-16 09:57:58 +1000

    Added required _toggleDrawerPanel function

--- elements/uqlibrary-element.js
+++ elements/uqlibrary-element.js
@@ -11,10 +11,25 @@
 			pageTitle: {
 				type: String,
 				value: 'Default title'
+			},
+			/**
+			 * Required. Whether the app should start in standalone mode or not.
+			 * @type Boolean
+			 */
+			standAlone: {
+				type: Object,
+				value: true
 			}
 		},
 		ready: function () {
 
+		},
+		/**
+		 * Toggles the drawer panel of the main UQL app
+		 * @private
+		 */
+		_toggleDrawerPanel: function () {
+			this.fire('uqlibrary-toggle-drawer');
 		}
 	})
 })();
